48f37b29f43c1eb641f391e0c537caa34ce7b041
Aditya Prasad <adityaprasad@vmware.com>
2016-06-30 12:17:37 +0530

    0.0.3

--- SwiftFramework/AwSwiftOperation.swift
+++ SwiftFramework/AwSwiftOperation.swift
@@ -18,7 +18,7 @@ public class AwSwiftOperation: NSObject {
         var result = AwOperation().getData() + "Swift"
         
     }
-    
+       
     public func Irm() {
        var x = MSProtectedData()
            x.length(nil)
